2df70c51b0ba8328375daac1183254d7ef9d222b
Tom <chanhinieong@gmail.com>
2025-08-15 00:38:57 +0800

    Clear unnecessary files

--- data/二級/main.py
+++
@@ -1,8 +0,0 @@
-years = [2021, 2022, 2023]
-months = [3, 6, 9, 12]
-level = 2
-
-for year in years:
-    for month in months:
-        file = open("Python_%d%02d_%d.json"%(year, month, level), 'a')
-        file.close()
